8b63da4818ae75a6a27df8f4a0d5548a9418be3d
nasrimohtadi@gmail.com <Awkward@009>
2020-03-20 22:17:11 +0100

    Changed File Path

--- src/main/java/com/springboot/springbootfileupload/controller/UploadController.java
+++ src/main/java/com/springboot/springbootfileupload/controller/UploadController.java
@@ -1,7 +1,10 @@
 package com.springboot.springbootfileupload.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -9,11 +12,12 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+@RestController
 public class UploadController {
 
     Map<String, Object> result = new HashMap<>();
     /// Receive message
-    @RequestMapping("/uploadFile")
+    @RequestMapping("/upload")
     public Map<String, Object> upload(@RequestParam("attach") MultipartFile file) throws IOException {
         // File info
         System.out.println("File name = "  + file.getOriginalFilename());
@@ -21,7 +25,7 @@ public class UploadController {
 
         // Save to disk
         // file path example 1) Windows c:/, 3) Mac ~/Documents/
-        String filePath = "~/Documents/";
+        String filePath = "E:/Training/";
         file.transferTo(new File(filePath + file.getOriginalFilename()));
         result.put("Success", true);
         return result;
